0e92c6f7f5d8490ff47e24a17d6b889ffe9f0926
timoBBD <159038883+timoBBD@users.noreply.github.com>
2024-03-08 15:22:46 +0200

    Feature/search filter update create (#7)
    * Creating folder layout
    
    * Basic Structure with DB connection working
    
    ---------
    
    Co-authored-by: Work Account <workaccount@Timos-MacBook-Air.local>

---
+++ backend/src/main/java/net/ryan/TimoRefactorLater/Controller/BeanController.java
@@ -0,0 +1,28 @@
+package net.ryan.TimoRefactorLater.Controller;
+
+import net.ryan.TimoRefactorLater.DTO.BeanDTO;
+import net.ryan.TimoRefactorLater.Service.BeanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BeanController {
+
+    BeanService beanService;
+
+    @Autowired
+    BeanController(BeanService beanService){
+        this.beanService = beanService;
+    }
+
+    @GetMapping(value = "/getBeans")
+    List<BeanDTO> test(){
+
+        return beanService.getBeans();
+
+    }
+
+}
