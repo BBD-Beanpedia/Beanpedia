69901b134b59f31e73ac8dfb85f9bbc91d19dc64
JC Mouton <jacques.mouton@bbd.co.za>
2024-03-11 13:39:07 +0200

    test route

--- backend/src/main/java/net/ryan/TimoRefactorLater/Controller/BeanController.java
+++ backend/src/main/java/net/ryan/TimoRefactorLater/Controller/BeanController.java
@@ -3,14 +3,18 @@ package net.ryan.TimoRefactorLater.Controller;
 import net.ryan.TimoRefactorLater.DTO.BeanDTO;
 import net.ryan.TimoRefactorLater.Service.BeanService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class BeanController {
     BeanService beanService;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @Autowired
     BeanController(BeanService beanService){
         this.beanService = beanService;
@@ -20,6 +24,12 @@ public class BeanController {
 
         return beanService.getBeans();
 
+    }
+
+    @GetMapping(value = "/test")
+    public List<Map<String, Object>> test() {
+        String sql = "SELECT * FROM \"BasicBeanInformation\"";
+        return jdbcTemplate.queryForList(sql);
     }
 
 }
