58f7eceaccf72fa688a7a960ad130c7bb86cdd80
LucindaBotes <Lucinda.Botes@bbd.co.za>
2024-03-12 19:03:50 +0200

    BEAN-44: Update all syntax

--- sql/updateScript.sql
+++ sql/updateScript.sql
@@ -8,65 +8,78 @@ ALTER COLUMN "BeanName" SET NOT NULL;
 
 -- changeset lJordaan:alter-table-basicbeaninformation-scientificname-non-nullable
 ALTER TABLE "BasicBeanInformation"
-ALTER COLUMN "ScientificName" varchar(50) NOT NULL;
--- rollback ALTER TABLE "BasicBeanInformation" ALTER COLUMN "ScientificName" varchar(50)
+ALTER COLUMN "ScientificName" TYPE varchar(50),
+ALTER COLUMN "ScientificName" SET NOT NULL;
+-- rollback ALTER TABLE "BasicBeanInformation" ALTER COLUMN "ScientificName" DROP NOT NULL;
 
 -- changeset lJordaan:alter-table-basicbeaninformation-beancontent-non-nullable
 ALTER TABLE "BasicBeanInformation"
-ALTER COLUMN "BeanContent" varchar(65535) NOT NULL;
--- rollback ALTER TABLE "BasicBeanInformation" ALTER COLUMN "BeanContent" varchar(65535)
+ALTER COLUMN "BeanContent" TYPE varchar(65535),
+ALTER COLUMN "BeanContent" SET NOT NULL;
+-- rollback ALTER TABLE "BasicBeanInformation" ALTER COLUMN "BeanContent" DROP NOT NULL;
 
 -- changeset lJordaan:alter-table-basicbeaninformation-originid-non-nullable
 ALTER TABLE "BasicBeanInformation"
-ALTER COLUMN "OriginId" integer NOT NULL;
--- rollback ALTER TABLE "BasicBeanInformation" ALTER COLUMN "OriginId" integer
+ALTER COLUMN "OriginId" TYPE integer,
+ALTER COLUMN "OriginId" SET NOT NULL;
+-- rollback ALTER TABLE "BasicBeanInformation" ALTER COLUMN "OriginId" DROP NOT NULL;
 
 -- changeset lJordaan:alter-table-basicbeaninformation-typeid-non-nullable
 ALTER TABLE "BasicBeanInformation"
-ALTER COLUMN "TypeId" integer NOT NULL;
--- rollback ALTER TABLE "BasicBeanInformation" ALTER COLUMN "TypeId" integer
+ALTER COLUMN "TypeId" TYPE integer,
+ALTER COLUMN "TypeId" SET NOT NULL;
+-- rollback ALTER TABLE "BasicBeanInformation" ALTER COLUMN "TypeId" DROP NOT NULL;
 
 -- changeset lJordaan:alter-table-basicbeaninformation-shapeid-non-nullable
 ALTER TABLE "BasicBeanInformation"
-ALTER COLUMN "ShapeId" integer NOT NULL;
--- rollback ALTER TABLE "BasicBeanInformation" ALTER COLUMN "ShapeId" integer
+ALTER COLUMN "ShapeId" TYPE integer,
+ALTER COLUMN "ShapeId" SET NOT NULL;
+-- rollback ALTER TABLE "BasicBeanInformation" ALTER COLUMN "ShapeId" DROP NOT NULL;
 
 -- changeset lJordaan:alter-table-basicbeaninformation-colourid-non-nullable
 ALTER TABLE "BasicBeanInformation"
-ALTER COLUMN "ColourId" integer NOT NULL;
--- rollback ALTER TABLE "BasicBeanInformation" ALTER COLUMN ColourId integer
+ALTER COLUMN "ColourId" TYPE integer,
+ALTER COLUMN "ColourId" SET NOT NULL;
+-- rollback ALTER TABLE "BasicBeanInformation" ALTER COLUMN "ColourId" DROP NOT NULL;
 
 -- changeset lJordaan:alter-table-beantype-beantype-non-nullable
 ALTER TABLE "BeanType"
-ALTER COLUMN "BeanType" varchar(50) NOT NULL;
--- rollback ALTER TABLE "BeanType" ALTER COLUMN "BeanType" varchar(50)
+ALTER COLUMN "BeanType" TYPE varchar(50),
+ALTER COLUMN "BeanType" SET NOT NULL;
+-- rollback ALTER TABLE "BeanType" ALTER COLUMN "BeanType" DROP NOT NULL;
 
 -- changeset lJordaan:alter-table-beantype-description-non-nullable
 ALTER TABLE "BeanType"
-ALTER COLUMN "Description" varchar(500) NOT NULL;
--- rollback ALTER TABLE "BeanType" ALTER COLUMN "Description" varchar(500)
+ALTER COLUMN "Description" TYPE varchar(500),
+ALTER COLUMN "Description" SET NOT NULL;
+-- rollback ALTER TABLE "BeanType" ALTER COLUMN "Description" DROP NOT NULL;
 
 -- changeset lJordaan:alter-table-beanshape-shape-non-nullable
 ALTER TABLE "BeanShape"
-ALTER COLUMN Shape varchar(50) NOT NULL;
--- rollback ALTER TABLE "BeanShape" ALTER COLUMN "Shape" varchar(50)
+ALTER COLUMN "Shape" TYPE varchar(50),
+ALTER COLUMN "Shape" SET NOT NULL;
+-- rollback ALTER TABLE "BeanShape" ALTER COLUMN "Shape" DROP NOT NULL;
 
 -- changeset lJordaan:alter-table-beanshape-description-non-nullable
 ALTER TABLE "BeanShape"
-ALTER COLUMN "Description" varchar(500) NOT NULL;
--- rollback ALTER TABLE "BeanShape" ALTER COLUMN "Description" varchar(500)
+ALTER COLUMN "Description" TYPE varchar(500),
+ALTER COLUMN "Description" SET NOT NULL;
+-- rollback ALTER TABLE "BeanShape" ALTER COLUMN "Description" DROP NOT NULL;
 
 -- changeset lJordaan:alter-table-beanorigin-origin-non-nullable
 ALTER TABLE "BeanOrigin"
-ALTER COLUMN "Origin" varchar(20) NOT NULL;
--- rollback ALTER TABLE "BeanOrigin" ALTER COLUMN "Origin" varchar(20)
+ALTER COLUMN "Origin" TYPE varchar(20),
+ALTER COLUMN "Origin" SET NOT NULL;
+-- rollback ALTER TABLE "BeanOrigin" ALTER COLUMN "Origin" DROP NOT NULL;
 
 -- changeset lJordaan:alter-table-beancolour-colour-non-nullable
 ALTER TABLE "BeanColour"
-ALTER COLUMN "Colour" varchar(20) NOT NULL;
--- rollback ALTER TABLE "BeanColour" ALTER COLUMN "Colour" varchar(20)
+ALTER COLUMN "Colour" TYPE varchar(20),
+ALTER COLUMN "Colour" SET NOT NULL;
+-- rollback ALTER TABLE "BeanColour" ALTER COLUMN "Colour" DROP NOT NULL;
 
 -- changeset lJordaan:alter-table-beancolour-description-non-nullable
 ALTER TABLE "BeanColour"
-ALTER COLUMN "Description" varchar(500) NOT NULL;
--- rollback ALTER TABLE "BeanColour" ALTER COLUMN "Description" varchar(500)
+ALTER COLUMN "Description" TYPE varchar(500),
+ALTER COLUMN "Description" SET NOT NULL;
+-- rollback ALTER TABLE "BeanColour" ALTER COLUMN "Description" DROP NOT NULL;
